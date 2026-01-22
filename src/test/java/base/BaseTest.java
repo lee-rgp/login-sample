365d9c0d11a97a3182f5980d7d940876f0048e40
lee-rgp <lee@rgp.asia>
2026-01-22 14:06:50 +0700

    Fix window size when run on CI/CD

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -43,6 +43,7 @@ public class BaseTest extends BasePage {
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
         }
 
         driver.set(new ChromeDriver(options));
